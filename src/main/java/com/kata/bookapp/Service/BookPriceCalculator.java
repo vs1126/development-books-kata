752051a6e00d487729b9a4da9dc7387e07b74d72
S <vaishnavi.e.s@capgemini.com>
2025-05-01 17:47:02 +0530

    added logic to calculate book price

--- src/main/java/com/kata/bookapp/Service/BookPriceCalculator.java
+++ src/main/java/com/kata/bookapp/Service/BookPriceCalculator.java
@@ -3,10 +3,35 @@ package com.kata.bookapp.Service;
 import com.kata.bookapp.dto.Book;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BookPriceCalculator {
 
     public double calculatePrice(List<Book> books) {
-        return books.size() * 50.0;
+
+        double bookPrice = 50.0;
+
+        Set<String> uniqueBookTitles = books.stream()
+                .map(Book::getTitle)
+                .collect(Collectors.toSet());
+
+        int differentBooks = uniqueBookTitles.size();
+        int totalBooks = books.size();
+
+        double totalPrice = totalBooks * bookPrice;
+
+        if(differentBooks == 2) {
+            totalPrice *= 0.95; // 5% discount
+        } else if(differentBooks == 3) {
+            totalPrice *= 0.90; //10% discount
+        } else if(differentBooks == 4) {
+            totalPrice *= 0.80; //20% discount
+        } else if(differentBooks == 5) {
+            totalPrice *= 0.75; // 25% discount
+        }
+
+
+        return totalPrice;
     }
 }
