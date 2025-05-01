42e712498b018515f867cb969f990265bb766c39
S <vaishnavi.e.s@capgemini.com>
2025-05-01 14:15:27 +0530

    Initial project setup

---
+++ src/test/java/com/kata/bookapp/BookappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.bookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
