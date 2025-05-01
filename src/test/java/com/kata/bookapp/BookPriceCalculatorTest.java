81e4897ea47f7f70e575266c8165800b5046fb62
S <vaishnavi.e.s@capgemini.com>
2025-05-01 15:16:42 +0530

    Added test cases for differnt books scenarios

--- src/test/java/com/kata/bookapp/BookPriceCalculatorTest.java
+++ src/test/java/com/kata/bookapp/BookPriceCalculatorTest.java
@@ -16,4 +16,46 @@ public class BookPriceCalculatorTest {
         List<Book> books = List.of(new Book("Book 1"));
         assertEquals(50.0, bookPriceCalculator.calculatePrice(books), 0.01);
     }
+
+    @Test
+    public void testTwoDifferentBooks(){
+        BookPriceCalculator bookPriceCalculator = new BookPriceCalculator();
+        List<Book> books = List.of(new Book("Book 1"), new Book("Book 2"));
+        assertEquals(95.0, bookPriceCalculator.calculatePrice(books), 0.01);
+
+    }
+
+    @Test
+    public void testThreeDifferentBooks(){
+        BookPriceCalculator bookPriceCalculator = new BookPriceCalculator();
+        List<Book> books = List.of(new Book("Book 1"),
+                new Book("Book 2"),
+                new Book("Book 3"));
+        assertEquals(135.0, bookPriceCalculator.calculatePrice(books), 0.01);
+    }
+
+    @Test
+    public void testFourDifferentBooks(){
+        BookPriceCalculator bookPriceCalculator = new BookPriceCalculator();
+        List<Book> books = List.of(
+                new Book("Book 1"),
+                new Book("Book 2"),
+                new Book("Book 3"),
+                new Book("Book 4"));
+        assertEquals(160.0, bookPriceCalculator.calculatePrice(books), 0.01);
+    }
+
+    @Test
+    public void testFiveDifferentBooks(){
+        BookPriceCalculator bookPriceCalculator = new BookPriceCalculator();
+        List<Book> books = List.of(
+                new Book("Book 1"),
+                new Book("Book 2"),
+                new Book("Book 3"),
+                new Book("Book 4"),
+                new Book("Book 5"));
+        assertEquals(187.5, bookPriceCalculator.calculatePrice(books), 0.01);
+    }
+
+
 }
